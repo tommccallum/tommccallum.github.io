f45bf092e8b6151f4a5a4fac7eb188210194af53
Tom McCallum <40837110+tommccallum@users.noreply.github.com>
2021-06-09 15:36:09 +0100

    30a3b8a

---
+++ _nuxt/static/1623248561/inspire/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftommccallum.github.io\u002F_nuxt\u002Fstatic\u002F1623248561",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002Ftommccallum.github.io\u002F",assetsPath:"\u002Ftommccallum.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
